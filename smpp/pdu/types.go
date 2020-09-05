9edf6e1de84f52b7893d1eb4d57c07f1f5e9194b
Babu.Jayaraj <babu.jayaraj@ril.com>
2020-09-05 15:51:19 +0530

    Changed the references to fiorix

--- smpp/pdu/types.go
+++ smpp/pdu/types.go
@@ -5,8 +5,8 @@
 package pdu
 
 import (
-	"github.com/fiorix/go-smpp/smpp/pdu/pdufield"
-	"github.com/fiorix/go-smpp/smpp/pdu/pdutlv"
+	"github.com/babuj/go-smpp/smpp/pdu/pdufield"
+	"github.com/babuj/go-smpp/smpp/pdu/pdutlv"
 )
 
 // PDU Types.
